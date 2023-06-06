0fe8d636606fa80c1f881835a8d4e386443b1ef2
Antony Potter <tone@potteringabout.net>
2023-06-06 14:47:21 +0000

    Added alb

---
+++ alb/main.tf
@@ -0,0 +1,30 @@
+variable "tags" {
+  type = object({
+    project         = string
+    account         = string
+    environment     = string
+    owner           = string
+    email           = string
+  })
+}
+
+variable "vpc_id" {}
+
+variable "alb" {
+  type = object({
+    name            = string
+    version         = string
+    subnet_ids      = string
+  })
+}
+
+variable "alb_subnets" {
+  type = list
+}
+
+module "alb" {
+  source          = "git@github.com:Allwyn-UK/plat-tfmod-alb.git?ref=${var.alb["version"]}"
+  name            = "${var.tags["project"]}-${var.tags["environment"]}-${var.alb["name"]}"
+  vpc_id          = var.vpc_id
+  alb_subnets     = var.alb["subnet_ids"] 
+}
