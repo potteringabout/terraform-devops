39ad47e0f8d71053d16d4768cab15c24c33c63a7
Antony Potter <tone@potteringabout.net>
2023-06-09 10:27:22 +0000

    Converted to terraform

--- main.tf
+++ main.tf
@@ -1,20 +1,38 @@
 locals {
-  state_key = "state/${local.project}/${local.account}/${local.environment}/${local.component}.tfstate"
+  project = "potteringabout"
+  account = "test"
+  environment = "test01"
+  accountid = "680805529666"
+  region = "eu-west-2"
+
+  state_key = "state/${local.project}/${local.account}/${local.environment}.tfstate"
   deployment_role  = "arn:aws:iam::${local.accountid}:role/${local.project}-${local.account}-github-deployment"
 }
 
+variable "region" {}
+
+variable "deployment_role" {}
+
 terraform {
   backend "s3" {
-    bucket         = "potteringabout-build"
-    key            = "${local.state_key}"
-    region         = "eu-west-2"
+    #bucket         = "potteringabout-build-${local.project}"
+    #key            = "tfstate"
+    #region         = "eu-west-2"
     encrypt        = true
-    dynamodb_table = "tflocks"
+    #dynamodb_table = "tflocks"
+    
   }
 }
 
 provider "aws" {
+  region = var.region
   assume_role {
-    role_arn = "${local.deployment_role}"
+    role_arn = var.deployment_role
   }
+}
+
+resource "aws_ssm_parameter" "my_param" {
+  name  = "foo2"
+  type  = "String"
+  value = "bar"
 }
