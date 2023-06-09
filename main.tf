fff4cd9e0c1249bb7e0d354227ecf0cea031b8c2
Antony Potter <tone@potteringabout.net>
2023-06-09 16:51:57 +0000

    Some updates

--- main.tf
+++ main.tf
@@ -22,12 +22,6 @@ variable "tags" {
   })
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0"
-    }
 
 terraform {
   required_providers {
