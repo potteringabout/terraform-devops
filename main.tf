a46c0badea13b0839691c026a2ec47bd05dbe404
Antony Potter <tone@potteringabout.net>
2023-06-09 16:18:18 +0000

    Some updates

--- main.tf
+++ main.tf
@@ -1,17 +1,26 @@
-locals {
-  project = "potteringabout"
-  account = "test"
-  environment = "test01"
-  accountid = "680805529666"
-  region = "eu-west-2"
+#locals {
+#  project = "potteringabout"
+#  account = "test"
+#  environment = "test01"
+#  accountid = "680805529666"
+#  region = "eu-west-2"
 
   #state_key = "state/${local.project}/${local.account}/${local.environment}.tfstate"
-  deployment_role  = "arn:aws:iam::${local.accountid}:role/${local.project}-${local.account}-github-deployment"
-}
+#  deployment_role  = "arn:aws:iam::${local.accountid}:role/${local.project}-${local.account}-github-deployment"
+#}*/
 
 variable "region" {}
 
-variable "deployment_role" {}
+variable "tags" {
+  type = object({
+    project         = string
+    account         = string
+    accountid       = string
+    environment     = string
+    owner           = string
+    email           = string
+  })
+}
 
 terraform {
   backend "s3" {
@@ -27,7 +36,7 @@ terraform {
 provider "aws" {
   region = var.region
   assume_role {
-    role_arn = var.deployment_role
+    role_arn = "arn:aws:iam::${var.tags["accountid"]}:role/${var.tags["project"]}-${var.tags["account"]}-github-deployment"
   }
 }
 
