07fbe888613cff6e0edc1d40017e4e27db77e619
Antony Potter <tone@potteringabout.net>
2023-10-18 21:18:59 +0000

    Debug

--- modules/s3/main.tf
+++ modules/s3/main.tf
@@ -1,7 +1,39 @@
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "bucket" {
   bucket = var.bucket_name
 
   tags = {
     Name        = var.bucket_name
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket_versioning" {
+  bucket = aws_s3_bucket.bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+
+
+resource "aws_s3_bucket_replication_configuration" "replication" {
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.source]
+
+  role   = aws_iam_role.replication.arn
+  bucket = aws_s3_bucket.source.id
+
+  rule {
+    id = "foobar"
+
+    filter {
+      prefix = "foo"
+    }
+
+    status = "Enabled"
+
+    destination {
+      bucket        = aws_s3_bucket.destination.arn
+      storage_class = "STANDARD"
+    }
   }
 }
